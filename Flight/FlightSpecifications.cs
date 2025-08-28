d96d95b6f17f1e3fc3c06e94f6093fe018196aa2
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-08-28 03:06:28 +0300

    Making the system more trustworthy by adding more tests that test different numbers

--- Flight/FlightSpecifications.cs
+++ Flight/FlightSpecifications.cs
@@ -12,6 +12,20 @@ namespace FlightTest
             flight.Book("nrmeen@c#.com", 1);
             flight.RemainingNumberOfSeats.Should().Be(2);
         }
+        [Fact]
+        public void Booking_reduces_the_number_of_seats_2()
+        {
+            var flight = new Domain.Flight(seatCapacity: 6);
+            flight.Book("nrmeen@c#.com", 3);
+            flight.RemainingNumberOfSeats.Should().Be(3);
+        }
+        [Fact]
+        public void Booking_reduces_the_number_of_seats_3()
+        {
+            var flight = new Domain.Flight(seatCapacity: 10);
+            flight.Book("nrmeen@c#.com", 4);
+            flight.RemainingNumberOfSeats.Should().Be(6);
+        }
 
         [Fact]
         public void Avoids_overbooking()
@@ -22,6 +36,16 @@ namespace FlightTest
             var error = flight.Book("nrmeen@c#.com", 4);
             //Then
             error.Should().BeOfType<OverbookingError>();
+        }
+        [Fact]
+        public void Books_flights_successfully()
+        {
+            //given 
+            var flight=new Domain.Flight(seatCapacity:3);
+            //when
+            var error=flight.Book("nrmeen@c#.com", 1);
+            //then 
+            error.Should().BeNull();
         }
     }
 }
