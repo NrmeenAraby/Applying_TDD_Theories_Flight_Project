658836f269e49e62b24ad0f188cf0fddff05e284
NrmeenAraby <115955249+NrmeenAraby@users.noreply.github.com>
2025-08-31 00:59:34 +0300

    Refactoring Booking Service

--- Application.Tests/FlightApplicationSpecifications.cs
+++ Application.Tests/FlightApplicationSpecifications.cs
@@ -23,45 +23,8 @@ namespace Application.Tests
             //then
             bookingService.FindBookings(flight.Id).Should().ContainEquivalentOf(new BookingRm(passengerEmail, numOfSeats));
         }
-        public class BookingService
-        {
-            public  Entities Entities { get; set; }
-            public BookingService(Entities entities) {
-                Entities = entities;
-            }
-            public void Book(BookDto bookDto)
-            {
-                var flight = Entities.flights.Find(bookDto.FlightId);
-                flight.Book(bookDto.PassengerEmail, bookDto.NumberOfSeats);
-                Entities.SaveChanges();
-            }
-            public IEnumerable<BookingRm> FindBookings(Guid flightId)
-            {
-                return Entities.flights.Find(flightId)
-                    .BookingList.Select(booking => new BookingRm(booking.Email, booking.NumberOfSeats));
-            }
-        }
-        public class BookDto
-        {
-            public Guid FlightId { get; set; }
-            public string PassengerEmail { get; set; }
-            public int NumberOfSeats { get; set; }
-            public BookDto(Guid flightId,string passengerEmail,int numberOfSeats) { 
-                FlightId=flightId;
-                PassengerEmail=passengerEmail;
-                NumberOfSeats=numberOfSeats;
-            }
-
-        }
-        public class BookingRm
-        {
-            public string PassengerEmail { get; set; }
-            public int NumberOfSeats { get; set; }
-            public BookingRm(string passengerEmail, int numberOfSeats)
-            {
-                PassengerEmail= passengerEmail;
-                NumberOfSeats=numberOfSeats;
-            }
-        }
+        
+       
+       
     }
 }
---
+++ Application/BookingService.cs
@@ -0,0 +1,24 @@
+﻿using Data;
+
+namespace Application
+{
+    public class BookingService
+    {
+        public Entities Entities { get; set; }
+        public BookingService(Entities entities)
+        {
+            Entities = entities;
+        }
+        public void Book(BookDto bookDto)
+        {
+            var flight = Entities.flights.Find(bookDto.FlightId);
+            flight.Book(bookDto.PassengerEmail, bookDto.NumberOfSeats);
+            Entities.SaveChanges();
+        }
+        public IEnumerable<BookingRm> FindBookings(Guid flightId)
+        {
+            return Entities.flights.Find(flightId)
+                .BookingList.Select(booking => new BookingRm(booking.Email, booking.NumberOfSeats));
+        }
+    }
+}
